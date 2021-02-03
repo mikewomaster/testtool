a0424ba881c96e3770d492a52c5d6ffb7f818f31
wangzonghao <mike.w@womtek.cn>
2021-02-03 09:20:16 +0800

    lmlc version number

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -39,11 +39,11 @@ MainWindow::MainWindow(QWidget *parent) :
 
     // ui->tabWidget->setTabEnabled(0, false);
     // ui->tabWidget->setTabEnabled(1, false);
-    // ui->tabWidget->setTabEnabled(2, false);
+    ui->tabWidget->setTabEnabled(2, false);
     ui->tabWidget->setTabEnabled(3, false);
     ui->tabWidget->setStyleSheet("QTabBar::tab:disabled {width: 0; color: transparent;}");
 
-    setWindowTitle("v2.1.0");
+    setWindowTitle("v2.1.1");
 }
 
 MainWindow::~MainWindow()
